751f24f9381d2aaf437e8322cd4b27e7d5118c46
SSong-develop <thdgnsrl123@naver.com>
2020-11-04 15:17:12 +0900

    initial commit :)

---
+++ app/src/main/java/com/example/sampleviewbinding/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.sampleviewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import com.example.sampleviewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.imageView.setOnClickListener {
+
+        }
+        binding.textView.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                TODO("Not yet implemented")
+            }
+        })
+        binding.button.setOnClickListener {
+            Toast.makeText(this,"hello World",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
